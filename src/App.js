92500a102d2566d358192b78f0c80238f10ea806
RocioMontero0803 <rocio.montero0803@gmail.com>
2021-04-26 01:09:32 -0500

    user input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
-import './App.css';
+const App = () => {
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
 
-function App() {
+  const getPokemon = async () => {
+    const toArray = []; //array
+    try {
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;//get indivdual pokemon from api
+      const res = await axios.get(url)
+      console.log(res);
+    }catch (e) { //error
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getPokemon();
+  }, [])
+
+  /*useEffect (() => {
+    axios.get('http://pokeapi.co/api/v2/pokemon')
+      .then((response) => {
+
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }) */
+
+const handleChange = (e) => {
+  setPokemon(e.target.value.toLowerCase())
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  getPokemon();
+}
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type="text" onChange={handleChange} placeholder = "Enter Pokemon Name"/>
+        </label>
+      </form>
       
     </div>
   );
-}
+};
 
 export default App;
