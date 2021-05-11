e40d1ff63f8a46cb6c3adf7dd57ab61a18110f26
RocioMontero0803 <rocio.montero0803@gmail.com>
2021-05-10 23:17:55 -0500

    pokedex container

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,6 +1,7 @@
-import React from "react";    
+/*import React from "react";    
 
 const CardsContainer = () => {
+
 React.useEffect(( )=>{
     console.log("in cards container")
 },[])
@@ -10,7 +11,122 @@ React.useEffect(( )=>{
         <h2>Cards Container</h2>
 
 
+
     );
 }
 
 export default CardsContainer
+*/import React, { useEffect, useState } from "react";
+import {
+    Grid,
+    Card,
+    CardMedia,
+    CardContent,
+    //Typography,
+    CircularProgress,
+   // Toolbar,
+    //AppBar,
+    //TextField,
+  } from "@material-ui/core";
+  import { fade, makeStyles } from "@material-ui/core/styles";
+  //import { toFirstCharUppercase } from "./constants";
+  //import SearchIcon from "@material-ui/icons/Search";
+  import axios from "axios";
+  
+  const useStyles = makeStyles((theme) => ({
+    pokedexContainer: {
+      paddingTop: "20px",
+      paddingLeft: "50px",
+      paddingRight: "50px",
+    },
+    cardMedia: {
+      margin: "auto",
+    },
+    cardContent: {
+      textAlign: "center",
+    },
+    searchContainer: {
+      display: "flex",
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      paddingLeft: "20px",
+      paddingRight: "20px",
+      marginTop: "5px",
+      marginBottom: "5px",
+    },
+    searchIcon: {
+      alignSelf: "flex-end",
+      marginBottom: "5px",
+    },
+    searchInput: {
+      width: "200px",
+      margin: "5px",
+    },
+  }));
+  
+  const CardsContainer = (props) => {
+    const classes = useStyles();
+    //const { history } = props;
+    const [pokemonData, setPokemonData] = useState({});
+    const [filter] = useState("");
+  
+    useEffect(() => {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon?limit=807`)
+        .then(function (response) {
+          const { data } = response;
+          const { results } = data;
+          const newPokemonData = {};
+          results.forEach((pokemon, index) => {
+            newPokemonData[index + 1] = {
+              id: index + 1,
+              name: pokemon.name,
+              sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                index + 1
+              }.png`,
+            };
+          });
+          setPokemonData(newPokemonData);
+        });
+    }, []);
+  
+    /*const handleSearchChange = (e) => {
+      setFilter(e.target.value);
+    };*/
+  
+    const getPokemonCard = (pokemonId) => {
+      const { sprite } = pokemonData[pokemonId];
+      return (
+        <Grid item xs={4} key={pokemonId}>
+          <Card >
+            <CardMedia
+              className={classes.cardMedia}
+              image={sprite}
+              style={{ width: "130px", height: "130px" }}
+            />
+            <CardContent className={classes.cardContent}>
+              
+            </CardContent>
+          </Card>
+        </Grid>
+      );
+    };
+  
+    return (
+      <>
+        
+        {pokemonData ? (
+          <Grid container spacing={2} className={classes.pokedexContainer}>
+            {Object.keys(pokemonData).map(
+              (pokemonId) =>
+                pokemonData[pokemonId].name.includes(filter) &&
+                getPokemonCard(pokemonId)
+            )}
+          </Grid>
+        ) : (
+          <CircularProgress />
+        )}
+      </>
+    );
+  };
+  
+  export default CardsContainer;
